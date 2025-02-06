15e7faa6014c2e39b74cf2b54046b863fa52a194
Vincent Lacoste <vinzlac@gmail.com>
2025-02-06 15:26:28 +0100

    suppression de Geist moins necessaire pour Nextjs 13

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1>Page d'accueil</h1>
+      <h1>Page d&apos;accueil</h1>
       {/* Migrez ici le contenu de votre ancien pages/index.mjs */}
     </main>
   );
